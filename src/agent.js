3330ce839aa6253f2f1f9b1f4e9b0b0926b21a6e
arbitrarylink <link@arbitraryexecution.com>
2021-09-24 13:16:11 -0400

    Added admin-events to agent.js the proper way

--- src/agent.js
+++ src/agent.js
@@ -2,14 +2,14 @@ const deployerWatchAgent = require('./deployer-watch');
 const adminEventsAgent = require('./admin-events/admin-events');
 
 const handleTransaction = async (txEvent) => {
-  const findings = [];
 
-  const [deployerWatchFindings] = await Promise.all([
-    deployerWatchAgent.handleTransaction(txEvent),
-    adminEventsAgent.handleTransaction(txEvent),
-  ]);
+  const findings = (
+    await Promise.all([
+      deployerWatchAgent.handleTransaction(txEvent),
+      adminEventsAgent.handleTransaction(txEvent),
+    ])
+  ).flat();
 
-  findings.push(...deployerWatchFindings);
   return findings;
 };
 
