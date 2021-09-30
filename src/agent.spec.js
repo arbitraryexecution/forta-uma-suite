fa93850eaf41e38f7efb2ed8884b18b4845133c8
kdawg <kdawg@arbitraryexecution.com>
2021-09-30 17:01:38 -0400

    updated agent.js and all the required directory changes

--- src/agent.spec.js
+++ src/agent.spec.js
@@ -1,50 +1,123 @@
-const { provideHandleTransaction } = require('./agent');
+const { provideInitialize, provideHandleTransaction, provideHandleBlock } = require('./agent');
 
 describe('agents', () => {
   let handleTransaction;
-  const mockAdminEventsAgent = {
+  let handleBlock;
+  let initialize;
+
+  const txHandler1 = {
     handleTransaction: jest.fn(),
+    initialize: jest.fn(),
   };
-  const mockDeployerWatchAgent = {
+  const txHandler2 = {
     handleTransaction: jest.fn(),
+    initialize: {},
   };
-  const mockMonitorMintCallsAgent = {
+  const txHandler3 = {
     handleTransaction: jest.fn(),
   };
   const mockTxEvent = {
     some: 'event',
   };
 
+  const blockHandler1 = {
+    handleBlock: jest.fn(),
+    initialize: jest.fn(),
+  };
+  const blockHandler2 = {
+    handleBlock: jest.fn(),
+    initialize: {},
+  };
+  const blockHandler3 = {
+    handleBlock: jest.fn(),
+  };
+  const mockBlockEvent = {
+    some: 'event',
+  };
+
   beforeAll(() => {
     handleTransaction = provideHandleTransaction([
-      mockAdminEventsAgent,
-      mockDeployerWatchAgent,
-      mockMonitorMintCallsAgent,
+      txHandler1,
+      txHandler2,
+      txHandler3,
+    ]);
+    handleBlock = provideHandleBlock([
+      blockHandler1,
+      blockHandler2,
+      blockHandler3,
+    ]);
+    initialize = provideInitialize([
+      txHandler1,
+      txHandler2,
+      txHandler3,
+      blockHandler1,
+      blockHandler2,
+      blockHandler3,
     ]);
   });
 
   describe('handleTransaction', () => {
-    it('invokes admin-events, deployer-watch and monitor-mint-calls agents and returns their findings', async () => {
+    it('invokes transaction handlers 1 through 3 and returns their findings', async () => {
       const mockFinding = { some: 'finding' };
-      mockAdminEventsAgent.handleTransaction.mockReturnValueOnce([mockFinding]);
-      mockDeployerWatchAgent.handleTransaction.mockReturnValueOnce([mockFinding]);
-      mockMonitorMintCallsAgent.handleTransaction.mockReturnValueOnce([mockFinding]);
+      txHandler1.handleTransaction.mockReturnValueOnce([mockFinding, mockFinding]);
+      txHandler2.handleTransaction.mockReturnValueOnce([mockFinding]);
+      txHandler3.handleTransaction.mockReturnValueOnce([]);
 
       const findings = await handleTransaction(mockTxEvent);
 
       expect(findings).toStrictEqual([mockFinding, mockFinding, mockFinding]);
-      expect(mockAdminEventsAgent.handleTransaction).toHaveBeenCalledTimes(1);
-      expect(mockAdminEventsAgent.handleTransaction).toHaveBeenCalledWith(
+      expect(txHandler1.handleTransaction).toHaveBeenCalledTimes(1);
+      expect(txHandler1.handleTransaction).toHaveBeenCalledWith(
         mockTxEvent,
       );
-      expect(mockDeployerWatchAgent.handleTransaction).toHaveBeenCalledTimes(1);
-      expect(mockDeployerWatchAgent.handleTransaction).toHaveBeenCalledWith(
+
+      expect(txHandler2.handleTransaction).toHaveBeenCalledTimes(1);
+      expect(txHandler2.handleTransaction).toHaveBeenCalledWith(
         mockTxEvent,
       );
-      expect(mockMonitorMintCallsAgent.handleTransaction).toHaveBeenCalledTimes(1);
-      expect(mockMonitorMintCallsAgent.handleTransaction).toHaveBeenCalledWith(
+
+      expect(txHandler3.handleTransaction).toHaveBeenCalledTimes(1);
+      expect(txHandler3.handleTransaction).toHaveBeenCalledWith(
         mockTxEvent,
       );
+    });
+  });
+
+  describe('handleBlock', () => {
+    it('invokes block handlers 1 through 3 and returns their findings', async () => {
+      const mockFinding = { some: 'finding' };
+      blockHandler1.handleBlock.mockReturnValueOnce([mockFinding, mockFinding]);
+      blockHandler2.handleBlock.mockReturnValueOnce([mockFinding]);
+      blockHandler3.handleBlock.mockReturnValueOnce([]);
+
+      const findings = await handleBlock(mockTxEvent);
+
+      expect(findings).toStrictEqual([mockFinding, mockFinding, mockFinding]);
+      expect(blockHandler1.handleBlock).toHaveBeenCalledTimes(1);
+      expect(blockHandler1.handleBlock).toHaveBeenCalledWith(
+        mockBlockEvent,
+      );
+
+      expect(blockHandler2.handleBlock).toHaveBeenCalledTimes(1);
+      expect(blockHandler2.handleBlock).toHaveBeenCalledWith(
+        mockBlockEvent,
+      );
+
+      expect(blockHandler3.handleBlock).toHaveBeenCalledTimes(1);
+      expect(blockHandler3.handleBlock).toHaveBeenCalledWith(
+        mockBlockEvent,
+      );
+    });
+  });
+
+  describe('initialize', () => {
+    it('invokes initialize function on all handlers that have valid initialize functions', async () => {
+      await initialize();
+
+      expect(txHandler1.initialize).toHaveBeenCalledTimes(1);
+      expect(txHandler1.initialize).toHaveBeenCalledWith();
+      expect(blockHandler1.initialize).toHaveBeenCalledTimes(1);
+      expect(blockHandler1.initialize).toHaveBeenCalledWith();
     });
   });
 });
--- src/deployer-watch/deployer-watch.js
+++ src/deployer-watch/deployer-watch.js
@@ -1,7 +1,7 @@
 const { Finding, FindingSeverity, FindingType } = require('forta-agent');
 
-const addresses = require('./config/deployer-watch.json');
-const config = require('../agent-config.json');
+const addresses = require('./deployer-watch.json');
+const config = require('../../agent-config.json');
 
 const deployerAddress = addresses.Deployer.toLowerCase();
 const whitelist = {};
