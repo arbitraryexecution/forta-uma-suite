d16891e7751a10ffc41eff6c4f467c5232d9dbd3
arbitrarylink <88783079+arbitrarylink@users.noreply.github.com>
2021-09-30 11:50:04 -0400

    AE-452: UMA admin events (#4)

--- src/agent.js
+++ src/agent.js
@@ -1,9 +1,11 @@
+const adminEventsAgent = require('./admin-events/admin-events');
 const deployerWatchAgent = require('./deployer-watch');
 const monitorMintCallsAgent = require('./monitor-mint-calls');
 
 const handleTransaction = async (txEvent) => {
   const findings = (
     await Promise.all([
+      adminEventsAgent.handleTransaction(txEvent),
       deployerWatchAgent.handleTransaction(txEvent),
       monitorMintCallsAgent.handleTransaction(txEvent),
     ])
